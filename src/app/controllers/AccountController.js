a68d1362d04ed574636283c75f964c0eca1867ff
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-03-17 07:55:33 -0300

    chore: add uuid

---
+++ src/app/controllers/AccountController.js
@@ -0,0 +1,7 @@
+class AccountController {
+  store(request, response) {
+    console.log('Estou no store');
+  }
+}
+
+module.exports = new AccountController();
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
-
 const router = Router();
+
+const AccountController = require('./app/controllers/AccountController');
+
+router.post('/accounts', AccountController.store);
 
 module.exports = router;
